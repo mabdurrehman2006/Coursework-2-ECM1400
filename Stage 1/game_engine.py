d8c65d7f9222bf93602b35d8aa2d07c6418fd9aa
Abdu <muhammadabduramen@gmail.com>
2025-12-02 14:38:58 +0000

    modified:   Stage 1/game_engine.py

--- Stage 1/game_engine.py
+++ Stage 1/game_engine.py
@@ -1,2 +1,24 @@
 def cli_coords_input():
-    coordinates=()
+    valid=[1, 2, 3, 4, 5, 6, 7, 8]
+    check=True
+    while check==True:
+        user_x=input("Enter X coordinate(Between 1 to 8)")
+        if user_x.isdigit():
+            if int(user_x) in valid:
+                check=False
+        else:
+            check=True
+    check=True
+    while check==True:
+        user_y=input("Enter Y coordinate(Between 1 to 8)")
+        if user_y.isdigit():
+            if int(user_y) in valid:
+                check=False
+        else:
+            check=True
+    coordinates=(int(user_x), int(user_y))
+    return coordinates
+
+
+coord=cli_coords_input()
+print(coord)
